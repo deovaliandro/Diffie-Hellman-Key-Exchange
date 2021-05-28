df577aff06d80e05262a41dbdb5e19d56dd8c9b9
deovaliandro <mdv16h@student.unhas.ac.id>
2021-05-28 14:48:49 +0800

    kampret betul, salah looping

--- diffie-hellman.cpp
+++ diffie-hellman.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <math.h>
 
+int powmod(int a, int b, int n) {
+    int result = a;
+    
+    for(int i=0; i<b-1; i++) {
+        result = (result * a) % n;
+    }
+
+    return result;
+}
+
 int isRelativePrime(int a, int b) {
     int dummy;
 
@@ -14,24 +24,29 @@ int isRelativePrime(int a, int b) {
 }
 
 int isPrimitiveRoot(int prime, int root) {
-    int h[prime-1];
+    int h[prime-2];
     int hprime = prime;
 
     for(int i=0; i<prime; i++) {
-        h[i] = std::pow(root, i+1);
+        h[i] = powmod(root, i+1, prime);
+        // std::cout << h[i] << std::endl;
     }
+    
+    int kampret = 1;
 
-    for(int i=0; i<sizeof(h)/sizeof(h[0]); i++) {
-        for (int j=0; j<sizeof(h)/sizeof(h[0]); j++) {
+    for(int i=0; i<sizeof(h)/sizeof(h[0]) - 1; i++) {
+        for (int j=kampret; j<sizeof(h)/sizeof(h[0]); j++) {
+            // std::cout << h[i] << "=" << h[j] << std::endl;
             if (h[i] == h[j]) {
+                std::cout << i << " " << j << " " << h[i] << " " << h[j] << std::endl;
                 std::cout << "Not primite root" << std::endl;
-                break;
+                return 0;
             }
         }
+            kampret++;
 
         if(isRelativePrime(prime, h[i]) != 1) {
-            std::cout << "Not primite root" << std::endl;
-            break;
+            return 0;
         }
     }
 
@@ -44,13 +59,12 @@ int main() {
     std::cin >> n >> g >> x >> y;
 
     if(isPrimitiveRoot(n, g) == 1) {
-        X = std::pow(g, x);
-        Y = std::pow(g, y);
-        X = X % n;
-        Y = Y % n;
 
-        k0 = pow(Y, x);
-        k1 = pow(X, y);
+        X = powmod(g, x, n);
+        Y = powmod(g, y, n);
+
+        k0 = powmod(Y, x, n);
+        k1 = powmod(X, y, n);
 
         if (k0 == k1) {
             std::cout << "Key same " << k0 << std::endl;
